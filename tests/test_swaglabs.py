4b2393b813902443042e51b3d6b4ca211cf5865a
LamijaMezit <lamija.mezit11@gmail.com>
2023-06-16 21:15:22 +0200

    verify_your_information_title

--- tests/test_swaglabs.py
+++ tests/test_swaglabs.py
@@ -9,6 +9,7 @@ def test_correct_login(driver):
     home_page.verification_your_cart_title()
     home_page.verification_products_in_the_chart()
     home_page.click_on_the_checkout()
+    home_page.verification_your_information_title()
     home_page.filling_the_fields("Lamija", "Mezit", "88400")
     home_page.verification_checkout_overview_title()
     home_page.verification_products_in_checkout_overview()
